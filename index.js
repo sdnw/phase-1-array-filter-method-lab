c7e35f76b13c156948d7ef41b5df26240157453d
Steven <stevendawn98@gmail.com>
2022-06-10 15:28:14 -0400

    commit tree bf18c4dbae35ff2cee2d42c85ebb77efc796ccf3

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+// function should take an array/string 
+function findMatching(array, string) {
+    // set iteration to a variable when filter loops through array
+    return array.filter(el =>
+        // return matching names, case insensitive
+         el.toLowerCase() === string.toLowerCase()
+         )
+}
+
+// function should take an array/string
+console.log('array: ', array, 'string: ', string)
+    function fuzzyMatch(array, string) {
+        // set iteration to a variable when filter loops through array
+        let filtered = array.filter(el => 
+            // match only beginning letters, ignore middle/ending letters
+            el.indexOf(string) === 0
+            )
+            console.log(filtered)
+            return filtered
+        
+}
+
+function matchName(array, string) {
+    let filtered = array.filter(el => 
+        el.name === string)
+    return filtered
+}
+
